16631247f15621476c2407fe696bc742d9878d31
Owen <ocotten24@basecampcodingacademy.org>
2023-11-02 11:36:51 -0500

    changed to 2 paths

--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ def all_teams_view(request: HttpRequest) -> HttpResponse:
         Team(
             'Management',
             'The Management Team plays a crucial role in ensuring the smooth operation of Basecamp Coding Academy. Their primary responsibilities include creating and updating chore schedules for the program, ensuring that students have assigned tasks and responsibilities. Additionally, they oversee the management of supplies and equipment, making sure that everything needed for daily activities is available.',
-            ['Owen','Jeremiah','Nick','Ab','Abigail','Matthew'] ),
+            ['Owen','Jeremiah','Nick','Ab','Abigail','Mathew'] ),
 
         Team(
             'Community',
@@ -35,44 +35,35 @@ def all_teams_view(request: HttpRequest) -> HttpResponse:
         ]}
     return render(request, "index.html", context)
 
+def team_detail_view(request: HttpRequest, name: str) -> HttpResponse:
+    team_info = None
 
-def procurement_team_view(request: HttpRequest) -> HttpResponse:
-    team_info = Team(
-            'Procurement',
-            'The Procurement Team focuses on the logistics of sustenance by handling the acquisition of food supplies for the program. They are responsible for purchasing the necessary ingredients and supplies to provide meals for students and staff. In addition to sourcing food, they also play a role in meal preparation, ensuring everyone is well-fed.',
-            ['Adrian', 'Bryce', 'Big John', 'Blaine', 'Wyatt']
+    if name == 'Management':
+        team_info = Team(
+            'Management',
+            'The Management Team plays a crucial role in ensuring the smooth operation of Basecamp Coding Academy. Their primary responsibilities include creating and updating chore schedules for the program, ensuring that students have assigned tasks and responsibilities. Additionally, they oversee the management of supplies and equipment, making sure that everything needed for daily activities is available.',
+            ['Owen', 'Jeremiah', 'Nick', 'Ab', 'Abigail', 'Matthew']
         )
-    
-    context = {'team': team_info}
-    return render(request, "teamDetails.html", context)
-
-def management_team_view(request: HttpRequest) -> HttpResponse:
-    team_info = Team(
-        'Management',
-        'The Management Team plays a crucial role in ensuring the smooth operation of Basecamp Coding Academy. Their primary responsibilities include creating and updating chore schedules for the program, ensuring that students have assigned tasks and responsibilities. Additionally, they oversee the management of supplies and equipment, making sure that everything needed for daily activities is available.',
-        ['Owen', 'Jeremiah', 'Nick', 'Ab', 'Abigail', 'Mathew']
-    )
-    
-    context = {'team': team_info}
-    return render(request, "teamDetails.html", context)
-    
-
-def documentation_team_view(request: HttpRequest) -> HttpResponse:
-    team_info = Team(
-            'Documentation',
-            "The Documentation Team is responsible for capturing and sharing the experiences and activities at Basecamp Coding Academy. They actively manage the program's social media presence and post pictures and videos that they take. Their role involves creating a visual record of the program's journey, sharing updates, and providing an insight into life at Basecamp Coding Academy through various social media platforms.",
-            ['Conner', 'Kaleigh', 'Blair', 'Mina', 'Jay', 'Joshua', 'Kayleah']
-        )
-    
-    context = {'team': team_info}
-    return render(request, "teamDetails.html", context)
-
-def community_team_view(request: HttpRequest) -> HttpResponse:
-    team_info = Team(
+    elif name == 'Community':
+        team_info = Team(
             'Community',
             'The Community Team is dedicated to fostering a sense of togetherness and engagement within Basecamp Coding Academy. They are responsible for organizing various events and activities that bring students and staff together. These events can range from social gatherings to bowling, all aimed at building a strong and supportive community within the program.',
             ['Jordan', 'Joby', 'Aj', 'Micah', 'Caleb']
         )
+    elif name == 'Documentation':
+        team_info = Team(
+            'Documentation',
+            "The Documentation Team is responsible for capturing and sharing the experiences and activities at Basecamp Coding Academy. They actively manage the program's social media presence and post pictures and videos that they take. Their role involves creating a visual record of the program's journey, sharing updates, and providing an insight into life at Basecamp Coding Academy through various social media platforms.",
+            ['Conner', 'Kaleigh', 'Blair', 'Mina', 'Jay', 'Joshua', 'Kayleah']
+        )
+    elif name == 'Procurement':
+        team_info = Team(
+            'Procurement',
+            'The Procurement Team focuses on the logistics of sustenance by handling the acquisition of food supplies for the program. They are responsible for purchasing the necessary ingredients and supplies to provide meals for students and staff. In addition to sourcing food, they also play a role in meal preparation, ensuring everyone is well-fed.',
+            ['Adrian', 'Bryce', 'Big John', 'Blaine', 'Wyatt']
+        )
+
     
+
     context = {'team': team_info}
     return render(request, "teamDetails.html", context)
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import all_teams_view, procurement_team_view, management_team_view, community_team_view, documentation_team_view
+from app.views import all_teams_view, team_detail_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", all_teams_view, name='home'),
-    path("team/Procurement", procurement_team_view, name='procurement'),
-    path("team/Management", management_team_view, name='management'),
-    path("team/Community", community_team_view, name='community'),
-    path("team/Documentation", documentation_team_view, name='documentation'),
+    path("team/<str:name>/", team_detail_view, name='team'), 
 ]

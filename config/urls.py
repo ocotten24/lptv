e8bd8d9b4ac7cf2489c665baad5396a18bab775f
Owen <ocotten24@basecampcodingacademy.org>
2023-11-02 13:10:05 -0500

    fixed paths..

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from app.views import all_teams_view, team_detail_view
 
 urlpatterns = [
     path("", all_teams_view, name='home'),
-    path("team/<str:name>/", team_detail_view, name='team'), 
+    path("<str:name>/", team_detail_view, name='team'), 
 ]
